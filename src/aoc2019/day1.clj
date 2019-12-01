23200eb52e45cb51c55b32deb1df682ad7117cf4
paul.rutledge <paul.rutledge@vodori.com>
2019-12-01 13:59:59 -0600

    cleanup

--- src/aoc2019/day1.clj
+++ src/aoc2019/day1.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io :as io]))
 
 (defn calculate-fuel1 [mass]
-  (Math/max (int 0) (int (- (Math/floor (/ mass 3.0)) 2))))
+  (int (Math/max (int 0) (int (- (Math/floor (/ mass 3.0)) 2)))))
 
 (defn calculate-fuel2 [mass]
   (loop [mass mass sum 0]
@@ -16,13 +16,11 @@
     (->> (line-seq reader)
          (map #(Long/parseLong %))
          (map calculate-fuel1)
-         (reduce +)
-         (int))))
+         (reduce +))))
 
 (defn problem2 []
   (with-open [reader (io/reader (io/resource "input1.txt"))]
     (->> (line-seq reader)
          (map #(Long/parseLong %))
          (map calculate-fuel2)
-         (reduce +)
-         (int))))
+         (reduce +))))
